78d9ea0e5cec290d8b0620c0dacbd92d0f12222d
paramitathecdrom <gparamita576@gmail.com>
2025-08-09 05:40:10 +0000

    container config done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let loggedIn = ""
+let BooleanLoggedIn = Boolean(loggedIn)
+console.log(BooleanLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Paramita" => true
+
+
+let someNo = 33
+let stringNo = String(someNo)
+console.log(stringNo)
+console.log(typeof stringNo)
